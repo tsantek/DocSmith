f7e3fd246e25209610eb4059814a333123eee289
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-07-23 15:45:40 -0700

    added reactstrap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
